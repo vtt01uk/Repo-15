c2932e84a0c70a505fcd2435db22bf885e0a9669
Meera <c.meera85@gmail.com>
2015-10-03 21:51:44 -0700

    Fixed bugs and tested

--- client/js/newkidController.js
+++ client/js/newkidController.js
@@ -1,31 +1,31 @@
-   var kids = angular.module('kids', ['ngRoute']);
-   
-   kids.factory('NewKidFactory',function($http){
-    var factory = {};
-    factory.addKids = function(info,callback){
-        $http.post('/add_kid', info).success(function(output){
-            callback(output);
-        });   
-    factory.getOrganisations = function(callback){
-        $http.get('/get_organisations').success(function(output){
-            callback(output);
-        });        
-    };
+  
+   saburiKonnect.factory('NewKidFactory',function($http){
+    	var factory = {};
+    	factory.addKid = function(info,callback){
+        	$http.post('/add_kid', info).success(function(output){
+            	callback(output);
+        	});   
+        };
+    	factory.getOrganisations = function(callback){
+        	$http.get('/get_organisations').success(function(output){
+            	callback(output);
+        	});        
+    	};
+    	return factory;
+    });
 
-    return factory;
-    };
-
-    kids.controller('NewKidsController', function($scope,$location,$routeParams, NewKidFactory){
-
+    saburiKonnect.controller('newKidController', function($scope,$location,$routeParams, NewKidFactory){
     	$scope.addKid = function()
     	{
+    		console.log("in controller");
     		NewKidFactory.addKid($scope.newkid);
     	};
 
-    NewKidFactory.getOrganisations(function(data)
-    {
-        $scope.organisations = data;
-    })
+    	NewKidFactory.getOrganisations(function(data)
+    	{
+       		$scope.organisations = data;
+    	})
+    });
 
-});
+
 
