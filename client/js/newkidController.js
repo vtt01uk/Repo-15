f3f8650764991ba0610c429df013cf9ce5b7eb35
Meera <c.meera85@gmail.com>
2015-10-03 19:50:52 -0700

    Created server-side controller for adding a new kid

--- client/js/newkidController.js
+++ client/js/newkidController.js
@@ -5,7 +5,14 @@
     factory.addKids = function(info,callback){
         $http.post('/add_kid', info).success(function(output){
             callback(output);
+        });   
+    factory.getOrganisations = function(callback){
+        $http.get('/get_organisations').success(function(output){
+            callback(output);
         });        
+    };
+
+    return factory;
     };
 
     kids.controller('NewKidsController', function($scope,$location,$routeParams, NewKidFactory){
@@ -14,5 +21,11 @@
     	{
     		NewKidFactory.addKid($scope.newkid);
     	};
-	});
+
+    NewKidFactory.getOrganisations(function(data)
+    {
+        $scope.organisations = data;
+    })
+
+});
 
--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,8 @@ module.exports = function(app){
     app.post('/add_kid',function(req,res){
     	kids.add_kid(req,res)
     });
+
+    app.get('/get_organisations',function(req,res){
+    	kids.get_organisations(req,res)
+    });
 }
